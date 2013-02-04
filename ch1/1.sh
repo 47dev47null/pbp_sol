aacbf05733a0bee2e5ec481221f14aa490e420d9
47dev47null <0x2fdev0x2fnull@gmail.com>
2013-02-04 11:49:44 +0800

    add Exercise 1-1

---
+++ ch1/1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Exercise 1-1
+# Write a script that creates a directory called bpl inside $HOME.
+# Populate this directory with two subdirectories, bin and scripts.
+
+mkdir $HOME/bpl
+mkdir $HOME/bpl/bin
+mkdir $HOME/bpl/scripts
